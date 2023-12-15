2ceb2ce451bb6f2227245975f23f441ff401ed17
aymanemontassir <aymane-hana@hotmail.fr>
2023-12-15 23:14:29 +0100

    fix issue

---
+++ nop.c
@@ -0,0 +1,6 @@
+#include "monty.h"
+void custom_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
--- push.c
+++ push.c
@@ -1,11 +1,5 @@
 #include "monty.h"
-/**
- * f_push - add node to the stack
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
-void f_push(stack_t **head, unsigned int counter)
+void custom_push(stack_t **head, unsigned int counter)
 {
 	int n, j = 0, flag = 0;
 
@@ -21,17 +15,17 @@ void f_push(stack_t **head, unsigned int counter)
 		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 			fclose(bus.file);
 			free(bus.content);
-			free_stack(*head);
+			custom_free_stack(*head);
 			exit(EXIT_FAILURE); }}
 	else
 	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
 		fclose(bus.file);
 		free(bus.content);
-		free_stack(*head);
+		custom_free_stack(*head);
 		exit(EXIT_FAILURE); }
 	n = atoi(bus.arg);
 	if (bus.lifi == 0)
-		addnode(head, n);
+		custom_addnode(head, n);
 	else
-		addqueue(head, n);
+		custom_addqueue(head, n);
 }
